b1d57472c775de5fd8aa1e94b37a073724f717d8
TommyNicolee <tommynicole2006@hotmail.com>
2025-12-03 14:06:30 -0500

    Récup params JSON

---
+++ TP2/Exercice_1/js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("http://localhost:8080/params_grille_nombres.php")
+        .then(response => response.json())
+        .then(data => {
+
+            //Récupération des paramètres du JSON
+            const params = data.parametres_tableau;
+
+            const nbColonnes = params.nombre_colonnes;
+            const nbLignes = params.nombre_lignes;
+
+            const min = params.plage_nombres_aleatoires.nombre_minimum;
+            const max = params.plage_nombres_aleatoires.nombre_maximum;
+
+            const seuil = params.nombre_base;
+
+            const couleurFond = params.couleur_fond_cellule;
+            const couleurTexte = params.couleur_texte_cellule;
+
+            
+        })
+});
