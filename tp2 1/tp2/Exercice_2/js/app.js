cecc4ed1b437f2dd2dc4a7237535c7c4e7a3ba35
TommyNicolee <tommynicole2006@hotmail.com>
2025-12-03 15:23:48 -0500

    Charger CDp et k

--- tp2 1/tp2/Exercice_2/js/app.js
+++ tp2 1/tp2/Exercice_2/js/app.js
@@ -2,3 +2,15 @@
 let CDP = [];   // Liste des CDp provenant du JSON
 let K = [];     // Liste des K provenant du JSON
 let forces = []; // Définition des forces
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Charger CDp et K
+    fetch("http://localhost:8080/data_cdp_k.php")
+        .then(r => r.json())
+        .then(data => {
+            CDP = data.data_cdp_k.cdp;  // [0.0206, 0.0465, 0.1386]
+            K   = data.data_cdp_k.k;    // [0.0364, 0.0334, 0.0301]
+        })
+
+});
